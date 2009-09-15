Fixed normalization bugs in MAP adaptation of HMM startprob and transmat.
Fixed small bugs in HMM unittests.

hmm_trainers.py
@@ -148,15 +148,14 @@ class BaseHMMBaumWelchTrainer(HMMTrainer):
 
 class GaussianHMMBaumWelchTrainer(BaseHMMBaumWelchTrainer):
     """Baum-Welch trainer for HMMs with Gaussian emissions."""
-
     emission_type = 'gaussian'
 
     def _initialize_sufficient_statistics(self, hmm):
         stats = super(GaussianHMMBaumWelchTrainer,
                       self)._initialize_sufficient_statistics(hmm)
-        stats['post']   = np.zeros(hmm._nstates)
-        stats['obs']    = np.zeros((hmm._nstates, hmm._ndim))
-        stats['obs**2'] = np.zeros((hmm._nstates, hmm._ndim))
+        stats['post']      = np.zeros(hmm._nstates)
+        stats['obs']       = np.zeros((hmm._nstates, hmm._ndim))
+        stats['obs**2']    = np.zeros((hmm._nstates, hmm._ndim))
         stats['obs*obs.T'] = np.zeros((hmm._nstates, hmm._ndim, hmm._ndim))
         return stats
 
@@ -218,6 +217,9 @@ class GaussianHMMMAPTrainer(GaussianHMMBaumWelchTrainer):
     def __init__(self, startprob_prior=None, transmat_prior=None,
                  means_prior=None, means_weight=1.0,
                  covars_prior=None, covars_weight=1.0):
+        """Initialize MAP trainer with the parameters for the prior
+        distribution.
+        """
         self.startprob_prior = startprob_prior
         self.transmat_prior = transmat_prior
         self.means_prior = means_prior
@@ -231,14 +233,15 @@ class GaussianHMMMAPTrainer(GaussianHMMBaumWelchTrainer):
             prior = self.startprob_prior
             if prior is None:
                 prior = 1.0
-            hmm.startprob = ((prior - 1.0 + stats['start'])
-                             / np.sum(prior - 1.0 + stats['start']))
+            hmm.startprob = normalize(np.maximum(prior - 1.0 + stats['start'],
+                                                 1e-20))
 
         if 't' in params:
             prior = self.transmat_prior
             if prior is None:
                 prior = 1.0
-            hmm.transmat = normalize(prior - 1.0 + stats['trans'], axis=1)
+            hmm.transmat = normalize(np.maximum(prior - 1.0 + stats['trans'],
+                                                1e-20), axis=1)
 
         denom = np.tile(stats['post'], (hmm._ndim, 1)).T
         if 'm' in params:
@@ -282,7 +285,7 @@ class GaussianHMMMAPTrainer(GaussianHMMBaumWelchTrainer):
                               - 2 * np.outer(stats['obs'][c], hmm._means[c])
                               + np.outer(hmm._means[c], hmm._means[c])
                               * stats['post'][c])
-                    cv_den = (max(weights_covar - hmm._ndim, 0)
+                    cv_den = (max(covars_weight - hmm._ndim, 0)
                               + stats['post'][c])
                     if hmm._cvtype == 'tied':
                         hmm._covars += ((covars_prior + cv_num)

tests/test_hmm.py
@@ -304,7 +304,7 @@ class GaussianHMMTester(GaussianHMMParams):
         samples = h.rvs(n)
         self.assertEquals(samples.shape, (n, self.ndim))
 
-    def test_train(self, params='stmc'):
+    def test_train(self, params='stmc', niter=5):
         h = hmm.GaussianHMM(self.nstates, self.ndim, self.cvtype)
         h.startprob = self.startprob
         tmp = self.transmat + np.diag(np.random.rand(self.nstates))
@@ -320,7 +320,7 @@ class GaussianHMMTester(GaussianHMMParams):
         h.init(train_obs, minit='points')
         init_testll = [h.lpdf(x) for x in test_obs]
 
-        trainll = h.train(train_obs, iter=50, params=params)
+        trainll = h.train(train_obs, iter=niter, params=params)
         if not np.all(np.diff(trainll) > -1e3):
             print
             print 'Test train: %s (%s)\n  %s\n  %s' % (self.cvtype, params,
@@ -328,11 +328,14 @@ class GaussianHMMTester(GaussianHMMParams):
         self.assertTrue(np.all(np.diff(trainll) > -1e3))
 
         post_testll = [h.lpdf(x) for x in test_obs]
-        if not (post_testll > init_testll):
+        if not (np.sum(post_testll) > np.sum(init_testll)):
             print
             print 'Test train: %s (%s)\n  %s\n  %s' % (self.cvtype, params,
                                                        init_testll, post_testll)
-        self.assertTrue(post_testll > init_testll)
+        self.assertTrue(np.sum(post_testll) > np.sum(init_testll))
+
+    def test_train_covars(self):
+        self.test_train('c')
 
 
 class TestGaussianHMMWithSphericalCovars(unittest.TestCase, GaussianHMMTester):
@@ -358,16 +361,16 @@ class TestGaussianHMMWithFullCovars(unittest.TestCase, GaussianHMMTester):
 
 
 class GaussianHMMMAPTrainerTester(GaussianHMMParams):
-    def test_train(self, params='stmc'):
+    def test_train(self, params='stmc', niter=5):
         covars_weight = 1.0
         if self.cvtype in ('full', 'tied'):
             covars_weight = self.ndim + 1.0
         trainer = hmm.hmm_trainers.GaussianHMMMAPTrainer(
-            startprob_prior=self.startprob,
-            transmat_prior=2*self.transmat,
+            startprob_prior=10*self.startprob + 2.0,
+            transmat_prior=10*self.transmat + 2.0,
             means_prior=self.means,
             means_weight=1.0,
-            covars_prior=self.covars,
+            covars_prior=self.covars[self.cvtype],
             covars_weight=covars_weight)
         h = hmm.GaussianHMM(self.nstates, self.ndim, self.cvtype,
                             trainer=trainer)
@@ -385,7 +388,7 @@ class GaussianHMMMAPTrainerTester(GaussianHMMParams):
         h.init(train_obs, minit='points')
         init_testll = [h.lpdf(x) for x in test_obs]
 
-        trainll = h.train(train_obs, iter=50, params=params)
+        trainll = h.train(train_obs, iter=niter, params=params)
         if not np.all(np.diff(trainll) > -1e3):
             print
             print 'Test MAP train: %s (%s)\n  %s\n  %s' % (self.cvtype, params,
@@ -393,12 +396,15 @@ class GaussianHMMMAPTrainerTester(GaussianHMMParams):
         self.assertTrue(np.all(np.diff(trainll) > -1e3))
 
         post_testll = [h.lpdf(x) for x in test_obs]
-        if not (post_testll > init_testll):
+        if not (np.sum(post_testll) > np.sum(init_testll)):
             print
             print 'Test MAP train: %s (%s)\n  %s\n  %s' % (self.cvtype, params,
                                                            init_testll,
                                                            post_testll)
-        self.assertTrue(post_testll > init_testll)
+        self.assertTrue(np.sum(post_testll) > np.sum(init_testll))
+
+    def test_train_covars(self):
+        self.test_train('c')
 
 
 class TestGaussianHMMMAPTrainerWithSphericalCovars(unittest.TestCase,